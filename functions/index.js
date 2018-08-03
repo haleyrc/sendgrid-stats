74857fd545836f69ffede2125d59e59d3df75b89
Ryan Haley <ryanhaley@frazer.biz>
2018-08-03 11:35:57 -0400

    Add an error handler and make sure to send all events

--- functions/index.js
+++ functions/index.js
@@ -4,18 +4,18 @@ var StatsD = require("hot-shots"),
   client = new StatsD({
     host: functions.config().statsd.ip,
     port: functions.config().statsd.port,
-    globalTags: { env: functions.config().project.env }
+    globalTags: { env: functions.config().project.env },
+    errorHandler: (err) => console.error("error during push:", err)
   })
 
 exports.sendgridEvent = functions.https.onRequest((req, res) => {
-  req.body.forEach((event) => {
-    // We send a different Datadog event type for each SendGrid event, allowing
-    // us to graph them separately.
-    client.increment(`email.${event.event}`, (error) => {
-      if (error) {
-        console.error("Error sending stats:", error)
-      }
-    })
+  const events = req.body.map((event) => `email.${event.event}`)
+  client.increment(events, (error) => {
+    if (error) {
+      console.error("Error sending stats:", error)
+    } else {
+      console.log(`Sent ${eventName} successfully.`)
+    }
+    res.status(200).end()
   })
-  res.status(200).end()
 })
